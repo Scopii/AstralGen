34f13a42e775528b7d0119033c60c1eb469ef669
Scopii <karimk2000@yahoo.de>
2025-05-24 02:07:38 +0200

    ver update for zig/vulkan

--- src/graphics_context.zig
+++ src/graphics_context.zig
@@ -71,7 +71,7 @@ pub const GraphicsContext = struct {
                 .application_version = @bitCast(vk.makeApiVersion(0, 0, 0, 0)),
                 .p_engine_name = app_name,
                 .engine_version = @bitCast(vk.makeApiVersion(0, 0, 0, 0)),
-                .api_version = @bitCast(vk.API_VERSION_1_2),
+                .api_version = @bitCast(vk.API_VERSION_1_3),
             },
             .enabled_extension_count = @intCast(extension_names.items.len),
             .pp_enabled_extension_names = extension_names.items.ptr,
