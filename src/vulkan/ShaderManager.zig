7e873c91b1a6ee5ba0aa2a725065e90da93fb720
Scopii <karimk2000@yahoo.de>
2025-12-07 19:56:54 +0100

    ShaderManager Improvements

--- src/vulkan/ShaderManager.zig
+++ src/vulkan/ShaderManager.zig
@@ -31,15 +31,24 @@ pub const ShaderManager = struct {
     pub fn init(alloc: Allocator, context: *const Context, resourceManager: *const ResourceManager) !ShaderManager {
         const gpi = context.gpi;
         const pipeLayout = try createPipelineLayout(gpi, resourceManager.descLayout, c.VK_SHADER_STAGE_ALL, @sizeOf(PushConstants));
-        var shaderObjects: [renderSeqLen]std.ArrayList(ShaderObject) = undefined;
-        var renderTypes: [renderSeqLen]RenderType = undefined;
 
+        var shaderObjects: [renderSeqLen]std.ArrayList(ShaderObject) = undefined;
+        for (0..renderSeqLen) |i| shaderObjects[i] = std.ArrayList(ShaderObject).init(alloc);
+
+        errdefer {
+            for (0..renderSeqLen) |i| {
+                for (shaderObjects[i].items) |*shaderObj| shaderObj.deinit(gpi);
+                shaderObjects[i].deinit();
+            }
+            c.vkDestroyPipelineLayout(gpi, pipeLayout, null);
+        }
+
+        var renderTypes: [renderSeqLen]RenderType = undefined;
         for (0..renderSeqLen) |i| {
             const renderPass = config.renderSeq[i];
-            const renderType = try checkShaderLayout(renderPass);
-            std.debug.print("ShaderLayout {} renderType {s} windowChannel {s} set\n", .{ i, @tagName(renderType), @tagName(renderPass.channel) });
-            shaderObjects[i] = try initShaderObjects(alloc, gpi, renderPass.shaders, resourceManager.descLayout, renderType);
-            renderTypes[i] = renderType;
+            renderTypes[i] = try checkShaderLayout(renderPass);
+            std.debug.print("ShaderLayout {} renderType {s} windowChannel {s} valid\n", .{ i, @tagName(renderTypes[i]), @tagName(renderPass.channel) });
+            shaderObjects[i] = try setShaderLayout(alloc, gpi, resourceManager.descLayout, i, renderTypes[i]);
         }
 
         return .{
@@ -52,78 +61,82 @@ pub const ShaderManager = struct {
         };
     }
 
-    pub fn getRenderType(self: *ShaderManager, sequenceIndex: usize) RenderType {
-        return self.renderTypes[sequenceIndex];
+    pub fn getRenderType(self: *ShaderManager, seqIndex: usize) RenderType {
+        return self.renderTypes[seqIndex];
     }
 
-    pub fn checkShaderLayout(shaderLayout: ShaderLayout) !RenderType {
-        var stages: [8]u8 = .{0} ** 8;
-        var prevIndex: i8 = -1;
-
-        for (shaderLayout.shaders) |shader| {
-            const curIndex: i8 = switch (shader.stage) {
-                c.VK_SHADER_STAGE_COMPUTE_BIT => 0,
-                c.VK_SHADER_STAGE_VERTEX_BIT => 1,
-                c.VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT => 2,
-                c.VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT => 3,
-                c.VK_SHADER_STAGE_GEOMETRY_BIT => 4,
-                c.VK_SHADER_STAGE_TASK_BIT_EXT => 5,
-                c.VK_SHADER_STAGE_MESH_BIT_EXT => 6,
-                c.VK_SHADER_STAGE_FRAGMENT_BIT => 7,
-                else => return error.UnknownShaderStage,
-            };
-            if (curIndex <= prevIndex) return error.ShaderLayoutOrderInvalid;
-            prevIndex = curIndex;
-            stages[@intCast(curIndex)] += 1;
-        }
-        switch (shaderLayout.shaders.len) {
-            1 => if (stages[0] == 1) return .compute else if (stages[1] == 1) return .vertOnly,
-            2 => if (stages[6] == 1 and stages[7] == 1) return .mesh,
-            3 => if (stages[5] == 1 and stages[6] == 1 and stages[7] == 1) return .taskMesh,
-            else => {},
-        }
-        if (stages[1] == 1 and stages[2] <= 1 and stages[3] <= 1 and stages[4] <= 1 and stages[5] == 0 and stages[6] == 0 and stages[7] == 1) return .graphics;
-        if (stages[2] != stages[3]) return error.ShaderLayoutTessellationMismatch;
-        return error.ShaderLayoutInvalid;
-    }
-
-    pub fn update(self: *ShaderManager, index: usize) !void {
-        const renderPass = config.renderSeq[index];
+    pub fn update(self: *ShaderManager, seqIndex: usize) !void {
+        const renderPass = config.renderSeq[seqIndex];
         const renderType = try checkShaderLayout(renderPass);
-        std.debug.print("ShaderLayout {} renderType {s} windowChannel {s} set\n", .{ index, @tagName(renderType), @tagName(renderPass.channel) });
+        std.debug.print("ShaderLayout {} renderType {s} windowChannel {s} updated\n", .{ seqIndex, @tagName(renderType), @tagName(renderPass.channel) });
 
-        var list = &self.shaderObjects[index];
+        const newList = try setShaderLayout(self.alloc, self.gpi, self.descLayout, seqIndex, renderType);
+
+        const list = &self.shaderObjects[seqIndex];
         for (list.items) |*shaderObject| shaderObject.deinit(self.gpi);
         list.deinit();
+        list.* = newList;
 
-        self.shaderObjects[index] = try initShaderObjects(self.alloc, self.gpi, config.renderSeq[index].shaders, self.descLayout, renderType);
-        self.renderTypes[index] = renderType;
+        self.renderTypes[seqIndex] = renderType;
     }
 
     pub fn deinit(self: *ShaderManager) void {
         const gpi = self.gpi;
 
-        for (self.shaderObjects) |shaderObjectList| {
-            for (shaderObjectList.items) |*shaderObject| shaderObject.deinit(gpi);
-            shaderObjectList.deinit();
+        for (&self.shaderObjects) |*shaderObjList| {
+            for (shaderObjList.items) |*shaderObj| shaderObj.deinit(gpi);
+            shaderObjList.deinit();
         }
         c.vkDestroyPipelineLayout(gpi, self.pipeLayout, null);
     }
 };
 
-fn initShaderObjects(alloc: Allocator, gpi: c.VkDevice, shaders: []const config.Shader, descLayout: c.VkDescriptorSetLayout, renderType: RenderType) !std.ArrayList(ShaderObject) {
-    var shaderObjects = std.ArrayList(ShaderObject).init(alloc);
+fn checkShaderLayout(shaderLayout: ShaderLayout) !RenderType {
+    var stages: [8]u8 = .{0} ** 8;
+    var prevIndex: i8 = -1;
+
+    for (shaderLayout.shaders) |shader| {
+        const curIndex: i8 = switch (shader.stage) {
+            c.VK_SHADER_STAGE_COMPUTE_BIT => 0,
+            c.VK_SHADER_STAGE_VERTEX_BIT => 1,
+            c.VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT => 2,
+            c.VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT => 3,
+            c.VK_SHADER_STAGE_GEOMETRY_BIT => 4,
+            c.VK_SHADER_STAGE_TASK_BIT_EXT => 5,
+            c.VK_SHADER_STAGE_MESH_BIT_EXT => 6,
+            c.VK_SHADER_STAGE_FRAGMENT_BIT => 7,
+            else => return error.UnknownShaderStage,
+        };
+        if (curIndex <= prevIndex) return error.ShaderLayoutOrderInvalid;
+        prevIndex = curIndex;
+        stages[@intCast(curIndex)] += 1;
+    }
+    switch (shaderLayout.shaders.len) {
+        1 => if (stages[0] == 1) return .compute else if (stages[1] == 1) return .vertOnly,
+        2 => if (stages[6] == 1 and stages[7] == 1) return .mesh,
+        3 => if (stages[5] == 1 and stages[6] == 1 and stages[7] == 1) return .taskMesh,
+        else => {},
+    }
+    if (stages[1] == 1 and stages[2] <= 1 and stages[3] <= 1 and stages[4] <= 1 and stages[5] == 0 and stages[6] == 0 and stages[7] == 1) return .graphics;
+    if (stages[2] != stages[3]) return error.ShaderLayoutTessellationMismatch;
+    return error.ShaderLayoutInvalid;
+}
+
+fn setShaderLayout(alloc: Allocator, gpi: c.VkDevice, descLayout: c.VkDescriptorSetLayout, index: usize, renderType: RenderType) !std.ArrayList(ShaderObject) {
+    var list = std.ArrayList(ShaderObject).init(alloc);
+    const shaders = config.renderSeq[index].shaders;
 
     for (0..shaders.len) |i| {
         const shader = shaders[i];
         const nextStage = if (i + 1 <= shaders.len - 1) shaders[i + 1].stage else 0;
         const shaderObj = try ShaderObject.init(gpi, shader, nextStage, alloc, descLayout, renderType);
-        shaderObjects.append(shaderObj) catch |err| {
+        list.append(shaderObj) catch |err| {
+            list.deinit();
             std.debug.print("ShaderManager could not append ShaderObject, err {}\n", .{err});
             return error.ShaderAppend;
         };
     }
-    return shaderObjects;
+    return list;
 }
 
 fn createPipelineLayout(gpi: c.VkDevice, descLayout: c.VkDescriptorSetLayout, pushConstantStages: c.VkShaderStageFlags, pushConstantSize: u32) !c.VkPipelineLayout {
