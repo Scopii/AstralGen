5ebc7c202b0ac499133fe9738f6ebf886e96ba5f
Scopii <karimk2000@yahoo.de>
2026-02-13 17:45:09 +0100

    Cleaned up Renderer.draw()

--- src/vulkan/sys/Renderer.zig
+++ src/vulkan/sys/Renderer.zig
@@ -112,22 +112,18 @@ pub const Renderer = struct {
 
     pub fn draw(self: *Renderer, frameData: FrameData) !void {
         if (rc.GPU_READBACK == true) try self.resMan.printReadbackBuffer(.{ .val = 45 }, vkT.ReadbackData, self.scheduler.flightId);
-
         const flightId = try self.scheduler.beginFrame();
+        if (rc.GPU_PROFILING == true) try self.renderGraph.cmdMan.printQueryResults(flightId);
+
         try self.resMan.cleanupResources(self.scheduler.totalFrames);
         const targets = try self.swapMan.getUpdatedTargets(flightId);
-
-        if (targets.len == 0) {
-            std.debug.print("\n\nFRAME SKIPPED!!\n\n", .{});
-            return;
-        }
-        if (rc.GPU_PROFILING == true) try self.renderGraph.cmdMan.printQueryResults(flightId);
 
         self.imguiMan.newFrame();
         self.imguiMan.drawUi();
 
         try self.resMan.descMan.updateDescriptors();
         const cmd = try self.renderGraph.recordFrame(self.passes.items, flightId, self.scheduler.totalFrames, frameData, targets, &self.resMan, &self.shaderMan, &self.imguiMan);
+        
         try self.scheduler.queueSubmit(cmd, targets, self.context.graphicsQ);
         try self.scheduler.queuePresent(targets, self.context.presentQ);
 
