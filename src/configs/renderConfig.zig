b364ed273c5b6dd6017d9a8f5c6f357fc3281faf
Scopii <karimk2000@yahoo.de>
2026-02-18 09:33:13 +0100

    Changed Resources to be UpdateType .Overwrite

--- src/configs/renderConfig.zig
+++ src/configs/renderConfig.zig
@@ -48,16 +48,16 @@ pub const TEX_COLOR_FORMAT = vk.VK_FORMAT_R16G16B16A16_SFLOAT;
 pub const TEX_DEPTH_FORMAT = vk.VK_FORMAT_D32_SFLOAT;
 
 // Buffers
-pub const indirectSB = BufferMeta.create(.{ .id = .{ .val = 41 }, .mem = .Gpu, .typ = .Indirect, .len = 1, .elementSize = @sizeOf(vhT.IndirectData), .update = .PerFrame });
-pub const readbackSB = BufferMeta.create(.{ .id = .{ .val = 45 }, .mem = .CpuRead, .typ = .Storage, .len = 1, .elementSize = @sizeOf(vhT.ReadbackData), .update = .Overwrite });
+pub const indirectSB = BufferMeta.create(.{ .id = .{ .val = 41 }, .mem = .Gpu, .typ = .Indirect, .len = 1, .elementSize = @sizeOf(vhT.IndirectData), .update = .PerFrame }); // Can be Overwrite?
+pub const readbackSB = BufferMeta.create(.{ .id = .{ .val = 45 }, .mem = .CpuRead, .typ = .Storage, .len = 1, .elementSize = @sizeOf(vhT.ReadbackData), .update = .PerFrame });
 
 pub const objectSB = BufferMeta.create(.{ .id = .{ .val = 1 }, .mem = .Gpu, .typ = .Storage, .len = 100, .elementSize = @sizeOf(Object), .update = .PerFrame });
 pub const cameraUB = BufferMeta.create(.{ .id = .{ .val = 40 }, .mem = .Gpu, .typ = .Uniform, .len = 1, .elementSize = @sizeOf(CameraData), .update = .PerFrame });
 pub const BUFFERS: []const BufferMeta.BufInf = &.{ objectSB, cameraUB, indirectSB, readbackSB };
 
 // Textures
-pub const quantTex = TextureMeta.create(.{ .id = .{ .val = 5 }, .mem = .Gpu, .typ = .Color, .width = 1920, .height = 1080, .update = .PerFrame });
-pub const quantDepthTex = TextureMeta.create(.{ .id = .{ .val = 11 }, .mem = .Gpu, .typ = .Depth, .width = 1920, .height = 1080, .update = .PerFrame });
+pub const quantTex = TextureMeta.create(.{ .id = .{ .val = 5 }, .mem = .Gpu, .typ = .Color, .width = 1920, .height = 1080, .update = .Overwrite }); // Can be Overwrite?
+pub const quantDepthTex = TextureMeta.create(.{ .id = .{ .val = 11 }, .mem = .Gpu, .typ = .Depth, .width = 1920, .height = 1080, .update = .Overwrite }); // Can be Overwrite?
 pub const TEXTURES: []const TextureMeta.TexInf = &.{ quantTex, quantDepthTex };
 
 // Passes
